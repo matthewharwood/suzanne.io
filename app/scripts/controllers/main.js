ee913014af5b66255b0a6ea06347fe885a84b83a
Daisy Lew <lew.daisy@gmail.com>
2014-12-21 20:03:47 -0800

    added tons of styles. got models done too

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,139 @@
  * # MainCtrl
  * Controller of the suzanneioApp
  */
-angular.module('suzanneioApp')
+var app = angular.module('suzanneioApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.experiences = [
+    	{
+    		timeframe: '2011 – Present',
+    		job: 'Marketing and Call Center / Dispatch Manager',
+    		location: 'Mediastream – Waycross, Georgia',
+    		list: [
+    			{detail:'Design and implement multimedia marketing campaigns working with marketing and sales team.'},
+    			{detail:'Call Center operations management including national Dispatch.'},
+    			{detail:'Supervise the interviewing, hiring and training of employees and the planning, assigning and directing of tasks and responsibilities.'},
+				{detail:'Develop and implement internal communications and training programs.'},
+				{detail:'Work with management team to develop budget.'},
+				{detail:'Evaluate financial reports to target areas of opportunity and growth.'}
+
+    		]
+    	},
+    	{
+    		timeframe: '2011 – Present',
+    		job: 'Marketing and Call Center / Dispatch Manager',
+    		location: 'Mediastream – Waycross, Georgia',
+    		list: [
+    			{detail:'Design and implement multimedia marketing campaigns working with marketing and sales team.'},
+    			{detail:'Call Center operations management including national Dispatch.'},
+    			{detail:'Supervise the interviewing, hiring and training of employees and the planning, assigning and directing of tasks and responsibilities.'},
+				{detail:'Develop and implement internal communications and training programs.'},
+				{detail:'Work with management team to develop budget.'},
+				{detail:'Evaluate financial reports to target areas of opportunity and growth.'}
+
+    		]
+    	},
+    	{
+    		timeframe: '2011 – Present',
+    		job: 'Marketing and Call Center / Dispatch Manager',
+    		location: 'Mediastream – Waycross, Georgia',
+    		list: [
+    			{detail:'Design and implement multimedia marketing campaigns working with marketing and sales team.'},
+    			{detail:'Call Center operations management including national Dispatch.'},
+    			{detail:'Supervise the interviewing, hiring and training of employees and the planning, assigning and directing of tasks and responsibilities.'},
+				{detail:'Develop and implement internal communications and training programs.'},
+				{detail:'Work with management team to develop budget.'},
+				{detail:'Evaluate financial reports to target areas of opportunity and growth.'}
+
+    		]
+    	},
+    	{
+    		timeframe: '2011 – Present',
+    		job: 'Marketing and Call Center / Dispatch Manager',
+    		location: 'Mediastream – Waycross, Georgia',
+    		list: [
+    			{detail:'Design and implement multimedia marketing campaigns working with marketing and sales team.'},
+    			{detail:'Call Center operations management including national Dispatch.'},
+    			{detail:'Supervise the interviewing, hiring and training of employees and the planning, assigning and directing of tasks and responsibilities.'},
+				{detail:'Develop and implement internal communications and training programs.'},
+				{detail:'Work with management team to develop budget.'},
+				{detail:'Evaluate financial reports to target areas of opportunity and growth.'}
+
+    		]
+    	}
+
     ];
+    // 
+    $scope.wtf = function(evt) {
+    	console.log('asdfasdf', evt);	
+    	// element.scrollTop()
+    };
+    
+  });
+app.factory('Experiences', function(){
+	var Experiences = {};
+	Experiences.status = [];
+	return Experiences;
+});
+app.directive('scrollSpyer', function ($window, Experiences) {
+    return {
+    	controller: function($scope){
+    		$scope.experiences = [];
+    		this.expMapper = function(offset, height) {
+    			$scope.experiences.push({'offset': offset, 'height': height, 'inView': false});
+    		};
+    	},
+    	link: function(scope) {
+    		var i = 0;
+	        angular.element($window).bind('scroll', function() {
+	        	
+	        	if(i < scope.experiences.length){
+	        		console.log(this.pageYOffset);
+        			if (this.pageYOffset >= scope.experiences[i].offset) {
+                 		scope.boolChangeClass = true;
+                 		Experiences.status.push(scope.experiences[i].inView=true);
+                 		console.log('boolif', scope.experiences[i].offset);
+                 		console.log('Experiences.status', Experiences.status);
+                 		i++;
+		            } else {
+		            	console.log('middle');
+		            }
+	        	} else {
+
+	        	}
+    			
+
+	            // console.log('boolif', scope.boolChangeClass, scope.experiences[i].offset);
+	            scope.$apply();
+	        });
+	    }
+	};
+});
+
+app.directive('spyed', function ($timeout) {
+    return {
+    	require: '^scrollSpyer',
+    	link: function(scope, el, attrs, ctrl) {
+    		$timeout(function(){
+    			var offset = el[0].getBoundingClientRect().top;
+			var theHeight = el[0].offsetHeight;
+			ctrl.expMapper(offset, theHeight);	
+    		},1000)
+			
+    		
+	    }
+	};
+});
+
+app.directive('inlineEdit', function() {
+    return {
+        restrict: 'EA',
+        // can be in-lined or async loaded by xhr
+        // or inlined as JS string (using template property)
+        templateUrl: 'componentTpl.html',
+        scope: {
+            model: '@',
+            version: '@',
+            subcaption: '@'
+          }
+        };
   });
